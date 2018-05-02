f593142eba4ec0c7096259eae4d0f69bc2fd5cb0
James Thomas <jamesjacobthomas7@gmail.com>
2018-05-02 18:44:12 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,24 @@
+
+
+ 
 def square_array(array)
   # your code here
-end
+    new_numbers = []
+  array.each do |num|
+    new_numbers << num ** 2
+  end
+    new_numbers
+end 
+
+
+
+ 
+def square_array2(array)
+  # your code here
+    
+  array.collect do |num|
+   num ** 2
+  end
+end 
+
+print square_array2([1,2,3])
